7d5d798f3411d788d51fa7e2ffa3bdc6069c0a4b
Krishna <krishnachaudhary2007gt@gmail.com>
2026-09-10 22:44:17 +0530

    remove Open Source section

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { GitBranch } from "lucide-react";
 
 import Build from "./components/Build";
 import Pack from "./components/Pack";
-import OpenSource from "./components/OpenSource";
 import GithubPage from "./components/Github";
 import Rest from "./components/Rest";
 import ThemeToggle from "./components/ThemeToggle";
@@ -97,7 +96,6 @@ export default function Home() {
 
       <Build />
       <Pack />
-      <OpenSource />
       <GithubPage />
       <Rest />
     </div>
